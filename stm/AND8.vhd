f300d595b05bf21d9fa7403c8886968bbe33b409
Vinicius De Carvalho <74793351+carvalhov@users.noreply.github.com>
2022-10-19 20:41:36 -0300

    Update and rename And8.vhd to AND8.vhd

--- stm/AND8.vhd
+++ stm/AND8.vhd
@@ -4,10 +4,10 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity AND8b is
 port(
      -- Port inputs 
-     EntA, EntB : in std_logic_vector(7 downto 0)
+     EntA, EntB : in std_logic_vector(7 downto 0);
      -- Port Outputs
      -- S0 é a saída de Op_Code 00 
-     S0          : out std_logic_vector(7 downto 0) 
+     S0          : out std_logic_vector(7 downto 0); 
 );
 end AND8b;
   
