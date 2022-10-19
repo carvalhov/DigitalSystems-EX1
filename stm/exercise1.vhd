79433e0b96baa14461be868e0f484dd0f9c5b394
Vinicius De Carvalho <74793351+carvalhov@users.noreply.github.com>
2022-10-19 11:51:27 -0300

    Update exercise1.vhd

--- stm/exercise1.vhd
+++ stm/exercise1.vhd
@@ -5,16 +5,15 @@ entity exercicio1 is
 
 port(
 
-		-- Input Ports  
-
-		clk    : in std_logic;
-		reset  : in std_logic;
+		-- Input Ports 
+    clk    : in std_logic;
+    reset  : in std_logic;
     OpCode : in std_logic_vector(1 downto 0);  
     Ent_A  : in std_logic_vector(7 downto 0);       
     Ent_B  : in std_logic_vector(7 downto 0); 
 
 		-- Output Ports
-		S:     out std_logic_vector(7 downto 0)
+    S:     out std_logic_vector(7 downto 0)
 
 );
 
