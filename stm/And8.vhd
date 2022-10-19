9648c01860d07aa79824e56afd2d2c9c3f6afd22
Vinicius De Carvalho <74793351+carvalhov@users.noreply.github.com>
2022-10-19 20:26:20 -0300

    Create And8.vhd

---
+++ stm/And8.vhd
@@ -0,0 +1,14 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity AND8b is
+port(
+     -- Port inputs 
+     EntA, EntB : in std_logic_vector(7 downto 0)
+     -- Port Outputs
+     -- S0 é a saída de Op_Code 00 
+     S0          : out std_logic_vector(7 downto 0) 
+);
+end AND8b;
+  
+
