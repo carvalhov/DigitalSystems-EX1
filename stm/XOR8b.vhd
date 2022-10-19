8b2ce3dc21b136a958e01f1e4198122b5d974e1d
Vinicius De Carvalho <74793351+carvalhov@users.noreply.github.com>
2022-10-19 20:29:38 -0300

    Update and rename XOR8d.vdh to XOR8b.vhd

---
+++ stm/XOR8b.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity XOR8b is
+port(
+     -- Port inputs 
+     EntA, EntB : in std_logic_vector(7 downto 0)
+     
+     -- Port Outputs
+     -- S4 é a saída de Op_Code: '11' 
+     S4          : out std_logic_vector(7 downto 0) 
+);
+end XOR8b;
