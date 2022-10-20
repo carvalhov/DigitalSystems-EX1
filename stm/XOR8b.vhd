17f09dd8a161a1ba6312b92fbbac97e4b5ec1528
Vinicius De Carvalho <74793351+carvalhov@users.noreply.github.com>
2022-10-19 21:18:09 -0300

    Update XOR8b.vhd

--- stm/XOR8b.vhd
+++ stm/XOR8b.vhd
@@ -13,6 +13,11 @@ port(
 end XOR8b;
      
 architecture Behavioral of XOR8b is
-
+     
+     -- Usar função map?
+     -- Esquema lógico 
+     -- S0[0] <= A[0] xor B[0];
+     -- S0[1] <= A[1] xor B[1]; 
+     -- idem ...
   
 end Behavioral;
