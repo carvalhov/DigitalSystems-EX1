a938a04a875d9196b119ea5e9e3b3e2740e0c674
Vinicius De Carvalho <74793351+carvalhov@users.noreply.github.com>
2022-10-21 08:40:37 -0300

    Update AND8.vhd

--- stm/AND8.vhd
+++ stm/AND8.vhd
@@ -4,7 +4,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity AND8b is
 port(
      -- Port inputs 
-     EntA, EntB : in std_logic_vector(7 downto 0);
+     A, B : in std_logic_vector(7 downto 0);
      -- Port Outputs
      -- S0 é a saída de Op_Code 00 
      S0          : out std_logic_vector(7 downto 0) 
@@ -13,13 +13,10 @@ port(
 end AND8b;
 
 architecture Behavioral of AND8b is
+  begin
+       
+    S0 <= A AND B;
      
-     -- Usar função map?
-     -- Esquema lógico 
-     -- S0[0] <= A[0] AND B[0];
-     -- S0[1] <= A[1] AND B[1]; 
-     -- idem ...
-  
 end Behavioral;
   
 
