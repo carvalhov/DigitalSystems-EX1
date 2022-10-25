f8229191836d9cb4261eb1626ca32c5cf1e115d1
Vinicius De Carvalho <74793351+carvalhov@users.noreply.github.com>
2022-10-25 18:19:40 -0300

    Update comparador8b.vhd

--- stm/op/comparador8b.vhd
+++ stm/op/comparador8b.vhd
@@ -6,20 +6,18 @@ port (
       -- Port inputs
       A, B               : in std_logic_vector(7 downto 0); 
       -- Port Output
-      -- AgB: A greater than B;    AeB: A equal B;    AlB: A lesser than B.
       S    : out std_logic -- Output = 1 when A >= B else 0
  );
 end comparador8b;
   
 architecture Behavioral of comparador8b is
-      signal: result     : std_logic;
 begin
       process(A, B)
-            IF (A>B or A=B) THEN  -- AgB: A greater than B;    AeB: A equal B;  
-                        result <= '1';
-            ELSE (A<B) THEN        -- AlB: A lesser than B.
-                        result <= '0';     
+			begin
+            if (A >= B) then  -- AgB: A greater than B;    AeB: A equal B;  
+                S <= '1' ;
+            elsif (A < B) then        -- AlB: A lesser than B.
+                S <= '0' ;     
             END IF;
-            s <= result;
       end process;
 end Behavioral;
