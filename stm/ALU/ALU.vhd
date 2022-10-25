b9b5c0f57d7f38787b9848f752c5db58db818de4
Vinicius De Carvalho <74793351+carvalhov@users.noreply.github.com>
2022-10-25 17:49:57 -0300

    Update ALU.vhd

--- stm/ALU/ALU.vhd
+++ stm/ALU/ALU.vhd
@@ -1,6 +1,5 @@
 library IEEE;
 use ieee.std_logic_1164.all;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use ieee.numeric_std.all;
 
 entity ALU is
@@ -29,18 +28,17 @@ signal tmp: std_logic_vector (8 downto 0);
 				 ALU_Result <= A AND B ;
 				
 			  when "010" => -- Componente Somador
-				 ALU_Result <= A + B ;
+				 ALU_Result <=  std_logic_vector(unsigned(A) + unsigned(B)) ;
 				
-			  when "100" => -- Componente subtrator (Lembrar que pode ser A-B ou B-A)
-				 if (A >= B) then -- Comparador de A e B
-					 ALU_Result <= A - B ;
-				 else
-					 ALU_Result <= B - A ;
-				 end if;
+			  when "100" => -- Componente subtrator A - B 
+				 ALU_Result <=  std_logic_vector(unsigned(A) - unsigned(B)) ;
 			 
 			  when "110" => -- Port XOR
 				 ALU_Result <= A XOR B ;
-				
+				 
+			  when "101" => -- Componente subtrator B - A 
+				 ALU_Result <=  std_logic_vector(unsigned(B) - unsigned(A)) ;
+					 
 			  when others => ALU_Result <= NULL; 
 			  end case;
 
