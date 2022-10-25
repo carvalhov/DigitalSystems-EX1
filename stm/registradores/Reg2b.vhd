1f6e8d9bc0befab212154070b28dc24ce4a315df
Vinicius De Carvalho <74793351+carvalhov@users.noreply.github.com>
2022-10-25 18:10:34 -0300

    Update Reg2b.vhd

--- stm/registradores/Reg2b.vhd
+++ stm/registradores/Reg2b.vhd
@@ -7,7 +7,8 @@ port(
 
 		-- Input Ports 
     clk     : in std_logic;
-	  reset   : in std_logic;
+	 reset   : in std_logic;
+	 ld      : in std_logic;
     X       : in std_logic_vector(1 downto 0);       
 
 		-- Output Ports
@@ -17,11 +18,12 @@ port(
 end Reg2b;
 	
 architecture behavioral of Reg2b is
+begin
 	process(Clk, reset, ld)
 	begin
 		if(reset = '1') then 
 			Y <= Null ;
-		elsif(risind_edge(clk) and load = '1') then 
+		elsif(rising_edge(clk) and ld = '1') then 
 		   Y <= X ;
 	   end if;
 	end process;
