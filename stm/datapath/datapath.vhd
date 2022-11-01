0a91efbdd53f05f6de3d36d583e22c05cbed772b
Vinicius De Carvalho <74793351+carvalhov@users.noreply.github.com>
2022-11-01 20:00:41 -0300

    Update datapath.vhd

--- stm/datapath/datapath.vhd
+++ stm/datapath/datapath.vhd
@@ -9,9 +9,12 @@ port(
 		-- Input Ports 
     clk    : in std_logic;
     reset  : in std_logic; -- Pode ser condiserado o Clear: tranformar em um load/clear no controlador
+	 in_ld1  : in std_logic;
+	 in_ld1  : in std_logic;
     OpCode : in std_logic_vector(1 downto 0);  
     Ent_A  : in std_logic_vector(7 downto 0);       
-    Ent_B  : in std_logic_vector(7 downto 0); 
+    Ent_B  : in std_logic_vector(7 downto 0);
+	 ALU_control: in std_logic_vector(2 downto 0);   
 
 		-- Output Ports
     Op     : out std_logic_vector(1 downto 0);  -- Saída do Registrador de OpCode para o controlador
@@ -24,6 +27,8 @@ end datapath;
 architecture behavioral of datapath is
    signal A       : std_logic_vector (7 downto 0);
    signal B       : std_logic_vector (7 downto 0);
+	signal S       : std_logic_vector (7 downto 0);
+	
 COMPONENT ALU
   port
  (
@@ -68,32 +73,45 @@ port (
       S      : out std_logic -- Output = 1 when A >= B else 0
 );
 end COMPONENT comparador_8b;	
-BEGIN
-  RegistradorA: Reg8b 
-	portmap (
-		clk => Clk;
-		X => Ent_A;
-		Y => A);
-  RegistradorB: Reg8b 
-	  portmap (
-		clk => Clk;
-		X => Ent_B;
-		Y => B);
-  Control_opcode: Reg2b 
-	  portmap (
-	 	clk => clk;
-	  	X => Opcode;
-	  	Y => Op);
-  comparadorAB: comparador_8b
-	  portmap(
-	  	A => A;
-		B => B;
-		S => Result);
-  ALU: ALU
-	  portmap(
-		A => A;
-		B => B;
-		ALU_Sel => ;-- Criar um sinal do controlador?
-		ALu_OUT => Saida);
 
+BEGIN
+   RegistradorA: Reg8b 
+	  port map (
+	   clk => clk,
+		reset => reset,
+		ld => in_ld1,
+		X => Ent_A,
+		Y => A);
+   RegistradorB: Reg8b 
+	  port map (
+	   clk => clk,
+		reset => reset,
+		ld => in_ld1,
+		X => Ent_B,
+		Y => B);
+   Control_opcode: Reg2b 
+	  port map (
+	   clk => clk,
+		reset => reset,
+		ld => in_ld1,
+	  	X => Opcode,
+	  	Y => Op);
+   comparadorAB: comparador_8b
+	  port map(
+	   A => A,
+		B => B,
+		S => Result);
+   ALU_EX1: ALU
+	  port map(
+	   A => A,
+		B => B,
+	   ALU_Sel => ALU_control, -- Criar um sinal do controlador?
+		ALU_OUT => S);
+   RegistradorS: Reg8b
+     port map(
+	   clk => clk,
+		reset => reset,
+		ld => in_ld2,
+		X => S,
+		Y => saida);
 end behavioral;
