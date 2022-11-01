60491caac4f61f3930fa30252c30ea5ac27db914
Vinicius De Carvalho <74793351+carvalhov@users.noreply.github.com>
2022-11-01 14:42:45 -0300

    Update datapath.vhd

--- stm/datapath/datapath.vhd
+++ stm/datapath/datapath.vhd
@@ -23,5 +23,50 @@ end datapath;
 	
 architecture behavioral of datapath is
 	
+COMPONENT ALU
+  port
+ (
+	  -- Input ports
+    A, B    : in  STD_LOGIC_VECTOR(7 downto 0);
+    ALU_Sel	: in std_logic_vector(2 downto 0);
+		
+    -- Output ports
+    ALU_OUT	: out STD_LOGIC_VECTOR(7 downto 0)
+);
+end COMPONENT ALU;
+
+COMPONENT Reg2b
+port(
+    -- Input Ports 
+    clk     : in std_logic; 
+    reset   : in std_logic;
+    ld      : in std_logic;
+    X       : in std_logic_vector(1 downto 0);       
+    -- Output Ports
+    Y       :  out std_logic_vector(1 downto 0)
+);
+end COMPONENT Reg2b;
+	
+COMPONENT Reg8b
+port(
+    -- Input Ports 
+    clk     : in std_logic;
+    reset   : in std_logic;
+    ld      : in std_logic;
+    X       : in std_logic_vector(7 downto 0);       
+    -- Output Ports
+    Y:     out std_logic_vector(7 downto 0)
+);
+end COMPONENT Reg8b;
+	
+COMPONENT comparador_8b
+port (
+      -- Port inputs
+      A, B   : in std_logic_vector(7 downto 0); 
+      -- Port Output
+      S      : out std_logic -- Output = 1 when A >= B else 0
+);
+end COMPONENT comparador_8b;	
+	
 	
 end behavioral;
