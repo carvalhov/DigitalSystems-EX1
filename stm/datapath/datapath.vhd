ca842b1bc61ebc6dd710984674a3f50d6c6664d6
Vinicius De Carvalho <74793351+carvalhov@users.noreply.github.com>
2022-10-31 23:02:32 -0300

    Update datapath.vhd

--- stm/datapath/datapath.vhd
+++ stm/datapath/datapath.vhd
@@ -1,1 +1,27 @@
 -- Datapath
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity datapath is
+
+port(
+
+		-- Input Ports 
+    clk    : in std_logic;
+    reset  : in std_logic;
+    OpCode : in std_logic_vector(1 downto 0);  
+    Ent_A  : in std_logic_vector(7 downto 0);       
+    Ent_B  : in std_logic_vector(7 downto 0); 
+
+		-- Output Ports
+    Op     : out std_logic_vector(1 downto 0);  -- Registrador de OpCode 
+    result : out std_logic;                     -- Resultado do comparador
+    Saída  : out std_logic_vector(7 downto 0)   -- Saída da ALU
+
+);
+end datapath;
+	
+architecture behavioral of datapath is
+	
+	
+end behavioral;
